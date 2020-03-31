2a0d0cd50cbe3e3c720b3bcc7772cd19e5c0eb3a
bopboy2 <scp0000@gmail.com>
2020-03-31 21:06:46 +0900

    03

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./Button.scss";
 import classNames from 'classnames';
 
-function Button({ children, size = 'medium' }) {
+function Button({ children, size, color }) {
     // return <button className={['Button', size].join(' ')}>{children}</button>;
-    return <button className={classNames('Button', size)}>{children}</button>;
+    return <button className={classNames('Button', size, color)}>{children}</button>;
 }
-// Button.defaultProps = { size: 'medium' };
+Button.defaultProps = { size: 'medium', color: 'blue' };
 export default Button;
